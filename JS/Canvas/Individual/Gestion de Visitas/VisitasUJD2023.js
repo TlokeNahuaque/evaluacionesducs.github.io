1f7a2ba08061838c976faeb3585b2058d2f27bcc
TlokeNahuaque <99765704+TlokeNahuaque@users.noreply.github.com>
2023-03-03 11:53:35 -0600

    Add files via upload

--- JS/Canvas/Individual/Gestion de Visitas/VisitasUJD2023.js
+++ JS/Canvas/Individual/Gestion de Visitas/VisitasUJD2023.js
@@ -7,7 +7,7 @@ var data1 = {
       strokeColor: "rgba(151,187,205,0.8)",
       highlightFill: "rgba(151,187,205,0.75)",
       highlightStroke: "rgba(151,187,205,1)",
-      data: [7.5]
+      data: [7.5,8.5]
 
 
     }
@@ -27,7 +27,7 @@ var data2 = {
       highlightFill: "rgba(220,220,220,0.75)",
       highlightStroke: "rgba(220,220,220,1)",
 
-      data: [421]
+      data: [421,619]
 
     }
   ]
