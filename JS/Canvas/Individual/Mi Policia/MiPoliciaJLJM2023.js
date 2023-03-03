b2e3c594b3684077e3026bcf65f0d4e3e6b65e50
TlokeNahuaque <99765704+TlokeNahuaque@users.noreply.github.com>
2023-03-03 12:48:45 -0600

    Add files via upload

--- JS/Canvas/Individual/Mi Policia/MiPoliciaJLJM2023.js
+++ JS/Canvas/Individual/Mi Policia/MiPoliciaJLJM2023.js
@@ -7,7 +7,7 @@ var data1 = {
       strokeColor: "rgba(151,187,205,0.8)",
       highlightFill: "rgba(151,187,205,0.75)",
       highlightStroke: "rgba(151,187,205,1)",
-      data: [5.8]
+      data: [5.8,7.8]
 
     }
   ]
@@ -26,7 +26,7 @@ var data2 = {
       highlightFill: "rgba(220,220,220,0.75)",
       highlightStroke: "rgba(220,220,220,1)",
 
-      data: [70]
+      data: [70,29]
 
     }
   ]
--- JS/Canvas/Individual/Mi Policia/MiPoliciaMVR2023.js
+++ JS/Canvas/Individual/Mi Policia/MiPoliciaMVR2023.js
@@ -7,7 +7,7 @@ var data1 = {
       strokeColor: "rgba(151,187,205,0.8)",
       highlightFill: "rgba(151,187,205,0.75)",
       highlightStroke: "rgba(151,187,205,1)",
-      data: [7.6,6.9]
+      data: [0,5.6]
 
     }
   ]
@@ -26,7 +26,7 @@ var data2 = {
       highlightFill: "rgba(220,220,220,0.75)",
       highlightStroke: "rgba(220,220,220,1)",
 
-      data: [452,366]
+      data: [0,2]
 
     }
   ]
